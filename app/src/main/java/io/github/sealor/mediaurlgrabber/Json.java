8cb7c59ee2626d3d9e276e1343c4bf91a3293ceb
Stefan Richter <sealor@users.noreply.github.com>
2018-07-29 15:58:50 +0200

    improved error logging

--- app/src/main/java/io/github/sealor/mediaurlgrabber/Json.java
+++ app/src/main/java/io/github/sealor/mediaurlgrabber/Json.java
@@ -3,54 +3,66 @@ package io.github.sealor.mediaurlgrabber;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
+import static java.lang.String.format;
+
 public class Json {
 
-    private final Object json;
+	private final Object json;
 
-    public Json(Object jsonObjectOrArray) {
-        assert jsonObjectOrArray instanceof JSONObject || jsonObjectOrArray instanceof JSONArray;
-        this.json = jsonObjectOrArray;
-    }
+	public Json(Object jsonObjectOrArray) {
+		assert jsonObjectOrArray instanceof JSONObject || jsonObjectOrArray instanceof JSONArray;
+		this.json = jsonObjectOrArray;
+	}
 
-    public String getString(String attributeName) {
-        JSONObject jsonObject = (JSONObject) this.json;
-        return (String) jsonObject.get(attributeName);
-    }
+	public String getString(String attributeName) {
+		JSONObject jsonObject = (JSONObject) this.json;
+		try {
+			return (String) jsonObject.get(attributeName);
+		} catch (Exception e) {
+			String message = format("string attribute '%s' not in '%s'", attributeName, jsonObject);
+			throw new RuntimeException(message, e);
+		}
+	}
 
-    public String getString(int index) {
-        JSONArray jsonArray = (JSONArray) this.json;
-        return (String) jsonArray.get(index);
-    }
+	public String getString(int index) {
+		JSONArray jsonArray = (JSONArray) this.json;
+		try {
+			return (String) jsonArray.get(index);
+		} catch (Exception e) {
+			String message = format("index '%d' not in '%s'", index, jsonArray);
+			throw new RuntimeException(message, e);
+		}
+	}
 
-    public Json getJson(String path) {
-        Json currentJson = this;
-        for (String pathFragment : path.split("[.\\[\\]]+")) {
-            Integer index = tryToParseInt(pathFragment);
+	public Json getJson(String path) {
+		Json currentJson = this;
+		for (String pathFragment : path.split("[.\\[\\]]+")) {
+			Integer index = tryToParseInt(pathFragment);
 
-            if (index != null) {
-                JSONArray jsonArray = (JSONArray) currentJson.json;
-                currentJson = new Json(jsonArray.get(index));
-            } else {
-                JSONObject jsonObject = (JSONObject) currentJson.json;
-                currentJson = new Json(jsonObject.get(pathFragment));
-            }
-        }
-        return currentJson;
-    }
+			if (index != null) {
+				JSONArray jsonArray = (JSONArray) currentJson.json;
+				currentJson = new Json(jsonArray.get(index));
+			} else {
+				JSONObject jsonObject = (JSONObject) currentJson.json;
+				currentJson = new Json(jsonObject.get(pathFragment));
+			}
+		}
+		return currentJson;
+	}
 
-    private Integer tryToParseInt(String integer) {
-        try {
-            return Integer.valueOf(integer);
-        } catch (NumberFormatException e) {
-            return null;
-        }
-    }
+	private Integer tryToParseInt(String integer) {
+		try {
+			return Integer.valueOf(integer);
+		} catch (NumberFormatException e) {
+			return null;
+		}
+	}
 
-    public String toString() {
-        if (this.json instanceof JSONArray)
-            return ((JSONArray) this.json).toJSONString();
-        else
-            return ((JSONObject) this.json).toJSONString();
-    }
+	public String toString() {
+		if (this.json instanceof JSONArray)
+			return ((JSONArray) this.json).toJSONString();
+		else
+			return ((JSONObject) this.json).toJSONString();
+	}
 
 }
