c472ddcd38c7881fb90ffcef89cc63b6b1a35403
Stefan Richter <sealor@users.noreply.github.com>
2018-09-01 16:31:59 +0200

    ARD/NDR Mediathek URL for testing updated

--- lib/src/test/java/io/github/sealor/mediaurlgrabber/lib/ArdMediathekTest.java
+++ lib/src/test/java/io/github/sealor/mediaurlgrabber/lib/ArdMediathekTest.java
@@ -1,9 +1,6 @@
 package io.github.sealor.mediaurlgrabber.lib;
 
-import org.json.simple.parser.ParseException;
 import org.junit.Test;
-
-import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
 
@@ -12,7 +9,7 @@ public class ArdMediathekTest {
 	public ArdMediathek ardMediathek = new ArdMediathek(null);
 
 	@Test
-	public void testArdMediathekInAllerFreundschaft() throws IOException, ParseException {
+	public void testArdMediathekInAllerFreundschaft() {
 		String videoUrl = "https://www.ardmediathek.de/tv/In-aller-Freundschaft-Die-jungen-%C3%84rzte/Folge-148-Kontrollverlust/Das-Erste/Video?bcastId=24966374&documentId=54549288";
 
 		String mp4Url = this.ardMediathek.grab(videoUrl);
@@ -22,7 +19,7 @@ public class ArdMediathekTest {
 	}
 
 	@Test
-	public void testDasErsteMediathekInAllerFreundschaft() throws IOException, ParseException {
+	public void testDasErsteMediathekInAllerFreundschaft() {
 		String videoUrl = "http://mediathek.daserste.de/In-aller-Freundschaft-Die-jungen-%C3%84rzte/Folge-148-Kontrollverlust/Video?bcastId=24966374&documentId=54549288";
 
 		String mp4Url = this.ardMediathek.grab(videoUrl);
@@ -32,12 +29,12 @@ public class ArdMediathekTest {
 	}
 
 	@Test
-	public void testInasNacht() throws IOException, ParseException {
-		String videoUrl = "https://www.ardmediathek.de/tv/Inas-Nacht/Inas-Nacht-mit-Bettina-Zimmermann-Doro-/Das-Erste/Video?bcastId=3411450&documentId=54609802";
+	public void testInasNacht() {
+		String videoUrl = "https://www.ardmediathek.de/tv/Inas-Nacht/Inas-Nacht-Bierdeckelfragen-an-Vicky-L/Das-Erste/Video?bcastId=3411450&documentId=54828258";
 
 		String mp4Url = this.ardMediathek.grab(videoUrl);
 
-		String expectedMp4Url = "https://mediandr-a.akamaihd.net/progressive/2018/0727/TV-20180727-1056-0200.hd.mp4";
+		String expectedMp4Url = "https://mediandr-a.akamaihd.net/progressive/2018/0726/TV-20180726-1030-1100.hd.mp4";
 		assertEquals(expectedMp4Url, mp4Url);
 	}
 
