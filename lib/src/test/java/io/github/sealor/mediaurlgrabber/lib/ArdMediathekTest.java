169ac3f204ff6f460ae8d7282601403b4683fd2f
Stefan Richter <sealor@users.noreply.github.com>
2018-10-03 13:42:08 +0200

    ArdMediathekTest now uses the currently provided videos for testing

--- lib/src/test/java/io/github/sealor/mediaurlgrabber/lib/ArdMediathekTest.java
+++ lib/src/test/java/io/github/sealor/mediaurlgrabber/lib/ArdMediathekTest.java
@@ -2,40 +2,83 @@ package io.github.sealor.mediaurlgrabber.lib;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static io.github.sealor.mediaurlgrabber.lib.helper.StringStartsWithRegex.startsWithRegex;
+import static org.junit.Assert.assertThat;
 
 public class ArdMediathekTest {
 
 	public ArdMediathek ardMediathek = new ArdMediathek(null);
 
 	@Test
-	public void testArdMediathekInAllerFreundschaft() {
-		String videoUrl = "https://www.ardmediathek.de/tv/In-aller-Freundschaft-Die-jungen-%C3%84rzte/Folge-148-Kontrollverlust/Das-Erste/Video?bcastId=24966374&documentId=54549288";
+	public void testArdMediathekInAllerFreundschaft() throws IOException {
+		String videoUrl = "https://www.ardmediathek.de";
+		try (InputStream is = openUrl("https://www.ardmediathek.de/tv/In-aller-Freundschaft/Sendung?documentId=13258516")) {
+			String inAllerFreundschaftMainPage = convertStreamToString(is);
+			videoUrl += resolveMatch(inAllerFreundschaftMainPage, "<a href=\"(/tv/In-aller-Freundschaft/[^\"]+)\" class=\"mediaLink\">");
+			videoUrl = decodeHtml(videoUrl);
+		}
 
 		String mp4Url = this.ardMediathek.grab(videoUrl);
 
-		String expectedMp4Url = "https://pdvideosdaserste-a.akamaihd.net/int/2018/06/25/1a6c11c3-a601-41f5-9041-eda48169a487/960-1.mp4";
-		assertEquals(expectedMp4Url, mp4Url);
+		assertThat(mp4Url, startsWithRegex("https://.*\\.akamaihd\\.net/.*\\.mp4"));
 	}
 
 	@Test
-	public void testDasErsteMediathekInAllerFreundschaft() {
-		String videoUrl = "http://mediathek.daserste.de/In-aller-Freundschaft-Die-jungen-%C3%84rzte/Folge-148-Kontrollverlust/Video?bcastId=24966374&documentId=54549288";
+	public void testDasErsteMediathekInAllerFreundschaft() throws IOException {
+		String videoUrl = "http://mediathek.daserste.de";
+		try (InputStream is = openUrl("http://mediathek.daserste.de/In-aller-Freundschaft/Sendung?documentId=6032920&topRessort&bcastId=6032920")) {
+			String inAllerFreundschaftMainPage = convertStreamToString(is);
+			videoUrl += resolveMatch(inAllerFreundschaftMainPage, "<a href=\"(/In-aller-Freundschaft/Folge-[^\"]+)\" class=\"mediaLink\">");
+			videoUrl = decodeHtml(videoUrl);
+		}
 
 		String mp4Url = this.ardMediathek.grab(videoUrl);
 
-		String expectedMp4Url = "https://pdvideosdaserste-a.akamaihd.net/int/2018/06/25/1a6c11c3-a601-41f5-9041-eda48169a487/960-1.mp4";
-		assertEquals(expectedMp4Url, mp4Url);
+		assertThat(mp4Url, startsWithRegex("https://.*\\.akamaihd\\.net/.*\\.mp4"));
 	}
 
 	@Test
-	public void testInasNacht() {
-		String videoUrl = "https://www.ardmediathek.de/tv/Inas-Nacht/Inas-Nacht-Bierdeckelfragen-an-Vicky-L/Das-Erste/Video?bcastId=3411450&documentId=54828258";
+	public void testArdMediathekInasNacht() throws IOException {
+		String videoUrl = "https://www.ardmediathek.de";
+		try (InputStream is = openUrl("https://www.ardmediathek.de/tv/Inas-Nacht/Sendung?documentId=52544614&bcastId=52544614")) {
+			String inAllerFreundschaftMainPage = convertStreamToString(is);
+			videoUrl += resolveMatch(inAllerFreundschaftMainPage, "<a href=\"(/tv/Inas-Nacht/.*/NDR-Fernsehen/[^\"]+)\" class=\"mediaLink\">");
+			videoUrl = decodeHtml(videoUrl);
+		}
 
 		String mp4Url = this.ardMediathek.grab(videoUrl);
 
-		String expectedMp4Url = "https://mediandr-a.akamaihd.net/progressive/2018/0726/TV-20180726-1030-1100.hd.mp4";
-		assertEquals(expectedMp4Url, mp4Url);
+		assertThat(mp4Url, startsWithRegex("https://.*\\.akamaihd\\.net/.*\\.mp4"));
+	}
+
+	private InputStream openUrl(String urlString) {
+		try {
+			URL url = new URL(urlString);
+			return url.openStream();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private String convertStreamToString(InputStream is) {
+		Scanner s = new Scanner(is).useDelimiter("\\A");
+		return s.hasNext() ? s.next() : "";
+	}
+
+	private String resolveMatch(String string, String pattern) {
+		Matcher m = Pattern.compile(pattern).matcher(string);
+		return m.find() ? m.group(1) : "";
+	}
+
+	private String decodeHtml(String html) {
+		return html.replaceAll("&amp;", "&");
 	}
 
 }
